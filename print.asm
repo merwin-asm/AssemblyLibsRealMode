454a714f84df2dff2275cf07f85b96d7d1afe805
Cactochan <85914469+mastercodermerwin@users.noreply.github.com>
2023-03-06 19:40:54 +0530

    Create print.asm

---
+++ print.asm
@@ -0,0 +1,11 @@
+;pass the string in bx
+print_string:
+     mov ah, 0x0e    ; function number = 0Eh : Display Character
+     mov al ,[bx] ; AL = code of character to display
+     int 0x10        ; call INT 10h, BIOS video service
+     inc bx
+     mov ax,[bx]
+     cmp ax , 0
+     jne print_string
+     ret
+
